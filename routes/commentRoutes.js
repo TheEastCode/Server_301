6776221820e5ced8527b0b901225612d7f86a1f8
Ekow Yawson <ekowyawson71@gmail.com>
2023-12-22 18:45:20 -0500

    Last edits

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -10,7 +10,7 @@ const {
 // const { protect } = require('../middleware/authMiddleware')
 const { protect } = require('../middleware/auth0')
 
-router.route('/comments').get(protect, getComments).post(protect, postComment)
+router.route('/').get(protect, getComments).post(protect, postComment)
 router.route('/:id').delete(protect, deleteComment).put(protect, updateComment)
 
 module.exports = router
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api/users', userRoutes)
 app.use('/api/goals', goalRoutes)
 app.use('/api/goals/:goalId/tasks', taskRoutes)
 app.use('/api/games/:userId', gameRoutes)
-app.use('/api/games/comments', commentRoutes)
+app.use('/api/comments', commentRoutes)
 app.use('/api/dalle', require('./routes/dalleRoute'))
 
 // Serve frontend
